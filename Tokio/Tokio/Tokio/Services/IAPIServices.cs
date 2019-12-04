3de803ed4e2f7e16bf5c836c8ab4450a29f6aa2b
Ari Christian <ari.christian@outlook.com>
2019-12-04 17:04:38 +0700

    Initial Commit

---
+++ Tokio/Tokio/Tokio/Services/IAPIServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tokio.Services
+{
+    public enum ApiMethod { POST = 0 , GET = 1 }
+    interface IAPIServices
+    {
+         Task CallApi<T>(ApiMethod method, string apiUrl,ICallback<T> callback, object body = null);
+    }
+
+    public interface ICallback<T>
+    {
+        void OnSuccess(T result);
+        void OnFailure(string message);
+        void OnStart();
+        void OnEnd();
+    }
+}
---
+++ Tokio/Tokio/Tokio/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Tokio.Models;
+
+namespace Tokio.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
